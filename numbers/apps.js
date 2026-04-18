fd5863dbcc8b0509b2495311ee1a94c519f9e636
Abbas-Developer <abbas.ghulam.hack@gmail.com>
2026-04-18 15:54:09 +0500

    practice of number

---
+++ numbers/apps.js
@@ -0,0 +1,54 @@
+//random number
+//round
+//floor
+//ceil
+
+
+
+
+
+
+// //Random number 0 to it will genrate random number for us 
+// console.log(Math.random()*10 );
+// console.log(Math.random()*100 );
+
+
+
+
+
+// //round    it will make it round for us 
+// const  num = "3.9"
+// console.log(Math.round(num));
+
+
+
+
+
+
+// //floor it will make it back the number it will written like in 3.5 it will make it back 
+// console.log(Math.floor(num));
+
+
+
+
+// //ceil
+//  console.log(Math.ceil(num));
+
+
+
+
+
+//  const str= "200";
+//  console.log(str)
+//  console.log(parseInt(str))
+//  console.log (Number(str));
+//  console.log (parseFloat(str));
+
+
+
+
+ const number= +prompt ("please enter your number ")
+ console.log(typeof "number");
+
+ const randomNumber = Math.floor((Math.random()*5))
+
