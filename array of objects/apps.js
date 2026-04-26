a4cad703e8a28e83307048b25a1e21f24795cbd6
mohammed-ahmed877 <abbas.ghulam.hack@gmail.com>
2026-04-26 16:58:47 +0500

    complet

---
+++ array of objects/apps.js
@@ -0,0 +1,130 @@
+// Array of object
+
+
+// 8. Create a new array containing only user names.
+// 9. Print users whose age is between 18 and 25.
+// 10. Print all user details one by one using a loop.
+
+const users = [
+    { id: 1, name: "Ali", age: 21, city: "Quetta" },
+    { id: 2, name: "Sara", age: 19, city: "Karachi" },
+    { id: 3, name: "Ahmed", age: 25, city: "Lahore" },
+    { id: 4, name: "Ayesha", age: 22, city: "Islamabad" },
+    { id: 5, name: "Usman", age: 24, city: "Peshawar" },
+    { id: 6, name: "Zainab", age: 20, city: "Multan" },
+    { id: 7, name: "Hassan", age: 23, city: "Faisalabad" },
+    { id: 8, name: "Fatima", age: 18, city: "Quetta" },
+    { id: 9, name: "Bilal", age: 26, city: "Hyderabad" },
+    { id: 10, name: "Noor", age: 21, city: "Sukkur" },
+];
+
+
+
+
+// const ol = document.querySelector("ol");
+// for (let i = 0; i < users.length; i++) {
+    //     console.log(users[i].city);
+    //      ol.innerHTML+=`<li>${users[i].city }</li>`
+    
+    //     }
+    
+    // const ol = document.querySelector("ol");
+    // for(let i= 0; i < users.length; i++){
+        //     console.log(users[i].name);
+        //      ol.innerHTML+=`<li>${users[i].city}</li>`
+        // }
+        
+        
+        // 1. Print all user names from the array.
+        // for(let i=0; i<users.length;i++){
+            //     console.log(users[i].name)
+            // }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //     // 2. Print all users’ ages.
+            // for (let i=0; i<users.length; i++){
+                //     console.log(users[i].age)
+                // }
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                // // 3. Print users whose age is greater than 20.
+                // for (let i = 0; i < users.length; i++) {
+                    
+                //     if(users[i].age >= 21)
+                //         console.log(users[i].age);
+                
+                
+                // }
+                
+                
+                
+                
+                
+                // // 4. Print users who are from "Quetta".
+                // for(let i =0; i< users.length; i++){
+                    //         if(users[i].city === "Quetta")
+                    //             console.log(users[i].city)
+                    //     }
+                    
+                    
+                    
+                    
+                    
+                    //     // 5. Print each user in this format: `"Name is Age years old"`.
+                    //    for(let i = 0; i < users.length; i++){
+                        //     console.log(`${users[i].name} is ${users[i]. age} years old.`);
+                        //    }
+                        
+                        
+                        
+                        
+                        //                     for(let i = 0; i < users.length; i++){
+                            //     // console.log(users[i]);
+                            //     console.log(`${users[i].name} is ${users[i].age} years old.`);
+                            
+                            
+                            // }
+                            
+                            
+                             
+                            
+                            
+                            // // 6. Count total number of users using a loop.
+                            // let count = 0;
+                            // for (let i = 0; i < users.length; i++) {
+                            //     count++;
+                                
+                                
+                            // }
+                            // console.log(count);
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            // // 7. Find and print the user with `id = 5`.
+                            // for(let i=0; i<users.length; i++){
+                            //     console.log(users[i]);
+                                
+                            // }
